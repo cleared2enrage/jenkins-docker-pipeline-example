194594eb46e7a32c94ff4fdcefe63c63b53606e0
Cleared2Enrage <jason.a.giles@gmail.com>
2017-03-18 14:27:53 -0400

    Now try to get the tests to pass and the pipeline to continue

--- tests.py
+++ tests.py
@@ -3,7 +3,7 @@ import unittest
 class ExampleTestClass(unittest.TestCase):
 
     def test_example_test_method(self):
-        self.assertTrue(False)
+        self.assertTrue(True)
 
 if __name__ == '__main__':
     unittest.main()
