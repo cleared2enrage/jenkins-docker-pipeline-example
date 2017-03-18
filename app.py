49541939bfe90c330df6e4368a357a8710f43e73
Cleared2Enrage <jason.a.giles@gmail.com>
2017-03-18 15:42:47 -0400

    Load balance two containers

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+import os
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    if os.environ['HOSTNAME']:
+        return 'Hello, from %s!' % os.environ['HOSTNAME']
     return 'Hello, World!'
 
 if __name__ == '__main__':
